2ce72668cf19f267c5f91707d36bfbd77d96cacf
Eyosiyas Bereketab <eyosiyas.b@suntecholding.com>
2021-12-11 16:22:38 +0300

    Splash screen implemented

--- app/src/main/java/com/myedu/view/MainActivity.kt
+++ app/src/main/java/com/myedu/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.myedu
+package com.myedu.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.myedu.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MyEdu);
         setContentView(R.layout.activity_main)
     }
 }
