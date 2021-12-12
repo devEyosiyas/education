7429bf6eb034b2e06218283ab257e21b04da0546
Eyosiyas Bereketab <eyosiyas.b@suntecholding.com>
2021-12-12 11:20:17 +0300

    Login UI completed

--- app/src/main/java/com/myedu/fragment/LoginFragment.kt
+++ app/src/main/java/com/myedu/fragment/LoginFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.myedu.R
 import com.myedu.databinding.FragmentLoginBinding
+import com.myedu.utils.Validator.validateEmail
+import com.myedu.utils.Validator.validateName
+import com.myedu.utils.Validator.validatePassword
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -22,6 +28,62 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.editEmail.doOnTextChanged { text, _, _, _ ->
+            if (validateName(text.toString()))
+                with(binding.emailInputLayout) {
+                    error = null
+                    isHelperTextEnabled = false
+                }
+            else
+                binding.emailInputLayout.error = getString(R.string.error_email)
+        }
+
+        binding.editPassword.doOnTextChanged { text, _, _, _ ->
+            if (validateName(text.toString()))
+                with(binding.passwordInputLayout) {
+                    error = null
+                    isHelperTextEnabled = false
+                }
+            else
+                binding.passwordInputLayout.error = getString(R.string.error_password)
+        }
+
+        binding.btnLogIn.setOnClickListener {
+            validateFields()
+            when {
+                !validateEmail(binding.editEmail.text.toString()) -> binding.editEmail.requestFocus()
+                !validatePassword(binding.editPassword.text.toString()) -> binding.editPassword.requestFocus()
+                else -> login()
+            }
+        }
+    }
+
+    private fun login() {
+        Toast.makeText(context, "Logged in", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun validateFields() {
+        if (!validateEmail(binding.editEmail.text.toString()))
+            with(binding.emailInputLayout) {
+                requestFocus()
+                error = getString(R.string.helper_email)
+            }
+        else
+            with(binding.emailInputLayout) {
+                error = null
+                isHelperTextEnabled = false
+            }
+
+        if (!validatePassword(binding.editPassword.text.toString()))
+            with(binding.passwordInputLayout) {
+                requestFocus()
+                error = getString(R.string.helper_password)
+            }
+        else
+            with(binding.passwordInputLayout) {
+                error = null
+                isHelperTextEnabled = false
+            }
 
     }
 
---
+++ app/src/main/java/com/myedu/utils/Validator.kt
@@ -0,0 +1,11 @@
+package com.myedu.utils
+
+object Validator {
+    fun validateName(name: String): Boolean = name.matches(Regex("^[a-zA-Z]{3,}+$"))
+
+    fun validateEmail(email: String): Boolean =
+        email.matches(Regex("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})\$"))
+
+    fun validatePassword(password: String): Boolean =
+        password.matches(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$"))
+}
